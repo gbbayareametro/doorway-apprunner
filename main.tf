77d5ddc97ef4d44985a3ab24dbeec1d9f3d9a080
Glenn Brown <gbrown@bayareametro.gov>
2024-02-26 11:52:33 -0800

    adding kms key

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ terraform {
     key            = "terraform/terraform.tfstate"
     region         = "us-west-2"
     encrypt        = true
+    kms_key_id = "arn:aws:kms:us-west-2:364076391763:key/965d66d2-a23f-4159-84a9-bf040e0b9e66"
   }
 }
 
