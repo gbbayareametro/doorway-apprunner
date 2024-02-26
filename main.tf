d7826d0802fdcb271cdc2acb59c1a8e8fb036865
Glenn Brown <gbrown@bayareametro.gov>
2024-02-26 11:14:52 -0800

    adding s3 tfstate

--- main.tf
+++ main.tf
@@ -6,9 +6,15 @@
 # module "network" {
 #   source = "./network"
 # }
+terraform {
+  backend "s3" {
+    bucket         = "dw-dev-bootstrap-s3-artifacts20240226000651036500000001"
+    key            = "terraform/terraform.tfstate"
+    region         = "us-west-2"
+    encrypt        = true
+  }
+}
 
 module "database" {
   source = "./modules/rds-aurora"
-  
-  
 }
