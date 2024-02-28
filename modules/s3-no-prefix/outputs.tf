216fa444d1c477e7cfc37cfb98607ee5d22bfbd9
Glenn Brown <gbrown@bayareametro.gov>
2024-02-28 15:37:56 -0800

    more backend encryption fixes

--- modules/s3-no-prefix/outputs.tf
+++ modules/s3-no-prefix/outputs.tf
@@ -1,5 +1,8 @@
 output "bucket" {
-    value = aws_s3_bucket.s3_bucket
+    value = aws_s3_bucket.s3_bucket.bucket
+}
+output "arn" {
+    value = aws_s3_bucket.s3_bucket.arn
 }
 
 output "encryption_key_arn" {
