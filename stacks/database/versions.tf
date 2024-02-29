8abf2285b062d92e183e6e332a776ce9e52536d1
Glenn Brown <gbrown@bayareametro.gov>
2024-02-29 15:41:57 -0800

    choose correct s3 bucket

--- stacks/database/versions.tf
+++ stacks/database/versions.tf
@@ -11,4 +11,11 @@ terraform {
       version = ">= 3.5"
     }
   }
+   backend "s3" {
+    bucket         = "dw-dev-bootstrap-s3-artifacts"
+    key            = "terraform/terraform.tfstate"
+    region         = "us-west-2"
+    encrypt        = true
+    kms_key_id = "arn:aws:kms:us-west-2:364076391763:key/e8bcda76-980f-42e5-97e5-12aae43c4fd9"
+  }
 }
