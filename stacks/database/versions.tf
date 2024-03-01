2b83c6cf72bbdf8b816c0ec7f5c342c8727fc837
Glenn Brown <gbrown@bayareametro.gov>
2024-03-01 15:21:26 -0800

    formatting

--- stacks/database/versions.tf
+++ stacks/database/versions.tf
@@ -1,6 +1,5 @@
 terraform {
   required_version = ">= 1.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -11,11 +10,11 @@ terraform {
       version = ">= 3.5"
     }
   }
-   backend "s3" {
-    bucket         = "oneoff-pipeline-s3-artifacts"
-    key            = "terraform/terraform.tfstate"
-    region         = "us-west-2"
-    encrypt        = true
+  backend "s3" {
+    bucket     = "oneoff-pipeline-s3-artifacts"
+    key        = "terraform/terraform.tfstate"
+    region     = "us-west-2"
+    encrypt    = true
     kms_key_id = "arn:aws:kms:us-west-2:364076391763:key/e8bcda76-980f-42e5-97e5-12aae43c4fd9"
   }
 }
