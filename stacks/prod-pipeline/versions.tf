0bff322a244e0c64d47da51a6b8a5ea792acb872
Glenn Brown <gbrown@bayareametro.gov>
2024-03-04 10:39:52 -0800

    fixing pipeline name and moving s3 backend

--- stacks/prod-pipeline/versions.tf
+++ stacks/prod-pipeline/versions.tf
@@ -11,10 +11,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dw-dev-bootstrap-s3-artifacts"
-    key            = "terraform/terraform.tfstate"
-    region         = "us-west-2"
-    encrypt        = true
+    bucket     = "dw-prod-infra-pipeline-s3-artifacts"
+    key        = "terraform/terraform.tfstate"
+    region     = "us-west-2"
+    encrypt    = true
     kms_key_id = "arn:aws:kms:us-west-2:364076391763:key/e8bcda76-980f-42e5-97e5-12aae43c4fd9"
   }
 }
