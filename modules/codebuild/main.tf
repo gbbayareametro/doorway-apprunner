c4de56e48a1e57e5e95363ba857b46ba37e5f789
Glenn Brown <gbrown@bayareametro.gov>
2024-03-16 15:03:49 -0700

    putting artifacts back to codepipeline

--- modules/codebuild/main.tf
+++ modules/codebuild/main.tf
@@ -1,11 +1,11 @@
-
+# trunk-ignore(checkov/CKV_AWS_147): No artifacts being passed at this point
 resource "aws_codebuild_project" "codebuild" {
   name          = var.name
   description   = var.description
   build_timeout = var.build_timeout
   service_role  = aws_iam_role.codebuild_role.arn
   artifacts {
-    type = "NO_ARTIFACTS"
+    type = "CODEPIPELINE"
   }
   environment {
     compute_type                = var.compute_type
