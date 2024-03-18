0ecdd435918b55f375b537b0b1999d77fd3224b7
Glenn Brown <gbrown@bayareametro.gov>
2024-03-18 14:15:00 -0700

    holding off on migration for a bit

--- modules/infra-pipeline/main.tf
+++ modules/infra-pipeline/main.tf
@@ -38,14 +38,14 @@ module "db_build" {
   app_name      = var.app_name
   environment   = each.key
 }
-module "db_migrator" {
-  for_each      = toset(var.build_envs)
-  source        = "../db_migrator"
-  name          = "${var.app_name}-${each.key}-db-migration"
-  environment   = each.key
-  pipeline_name = var.name
-  log_bucket    = module.log_bucket.bucket
-}
+# module "db_migrator" {
+#   for_each      = toset(var.build_envs)
+#   source        = "../db_migrator"
+#   name          = "${var.app_name}-${each.key}-db-migration"
+#   environment   = each.key
+#   pipeline_name = var.name
+#   log_bucket    = module.log_bucket.bucket
+# }
 resource "aws_codepipeline" "infra-pipeline" {
   role_arn = aws_iam_role.codepipeline_role.arn
   name     = var.name
@@ -103,18 +103,18 @@ resource "aws_codepipeline" "infra-pipeline" {
           ProjectName = module.db_build[var.build_envs[stage.key]].name
         }
       }
-      action {
-        name            = "DatabaseMigration"
-        category        = "Build"
-        owner           = "AWS"
-        provider        = "CodeBuild"
-        input_artifacts = ["infra-source"]
-        version         = "1"
-        run_order       = 2
-        configuration = {
-          ProjectName = module.db_migrator[var.build_envs[stage.key]].name
-        }
-      }
+      # action {
+      #   name            = "DatabaseMigration"
+      #   category        = "Build"
+      #   owner           = "AWS"
+      #   provider        = "CodeBuild"
+      #   input_artifacts = ["infra-source"]
+      #   version         = "1"
+      #   run_order       = 2
+      #   configuration = {
+      #     ProjectName = module.db_migrator[var.build_envs[stage.key]].name
+      #   }
+      # }
     }
 
   }
