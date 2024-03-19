c45342b99cad5f32e8a2ea17afd24ccda1cd7fc9
Glenn Brown <gbrown@bayareametro.gov>
2024-03-19 13:23:13 -0700

    adding parms

---
+++ modules/database/parameters.tf
@@ -0,0 +1,16 @@
+
+
+resource "aws_ssm_parameter" "db_host" {
+  name        = "/${var.app_name}/pipelines/${var.name}/${var.environment}/db/host"
+  type        = "SecureString"
+  value       = module.aurora_postgresql_v2.cluster_endpoint
+  description = "The bucket for the tf state files for this pipeline"
+  key_id      = var.ssm_encryption_key
+}
+resource "aws_ssm_parameter" "db_port" {
+  name        = "/${var.app_name}/pipelines/${var.name}/${var.environment}/db/port"
+  type        = "SecureString"
+  value       = module.aurora_postgresql_v2.cluster_port
+  description = "The bucket for the tf state files for this pipeline"
+  key_id      = var.ssm_encryption_key
+}
