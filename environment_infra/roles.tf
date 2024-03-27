48bf2ff5c488a62d6c595e07e9c886c79c9aeec3
Glenn Brown <gbrown@bayareametro.gov>
2024-03-27 12:15:16 -0700

    fatfinger

--- environment_infra/roles.tf
+++ environment_infra/roles.tf
@@ -30,7 +30,7 @@ data "aws_iam_policy_document" "apprunner-access" {
       aws_ssm_parameter.vpc_id.arn,
       data.aws_ssm_parameter.sendgrid.arn,
       module.aurora_postgresql_v2.cluster_master_user_secret[0].secret_arn,
-      module.module.aurora_postgresql_v2.master_user_secret_kms_key_id
+      module.aurora_postgresql_v2.master_user_secret_kms_key_id
     ]
   }
 
