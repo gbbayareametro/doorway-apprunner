8a4af56d999c25880ef4f85042430504743c22fa
Glenn Brown <gbrown@bayareametro.gov>
2024-03-26 17:00:51 -0700

    forgot arn

--- environment_infra/api.tf
+++ environment_infra/api.tf
@@ -7,7 +7,7 @@ resource "aws_apprunner_connection" "github" {
 resource "aws_apprunner_service" "service" {
   service_name = local.api_name
   instance_configuration {
-    instance_role_arn = aws_iam_role.apprunner_role
+    instance_role_arn = aws_iam_role.apprunner_role.arn
   }
   source_configuration {
     authentication_configuration {
